372540b0cfdfd4525f35e8fa127646e4ce0d50cb
nguyendangcode <dinh2671@gmail.com>
2025-08-10 07:55:39 +0000

    phan_so

--- phan_so.cpp
+++ phan_so.cpp
@@ -3,9 +3,14 @@
 void phan_so::in_phan_so(int tu,int mau){
     std::cout<<"phan so la: %d/%d\n"<<tu,mau;
 }
-void phan_so::nhan_phan_so(int tu1,int mau1,int tu2,int mau2){
-    in_phan_so(tu1*tu2,mau1*mau2);
-}
+int tu;   
+int mau;  
+phan_so NhanPhanSo(phan_so a) {
+        phan_so kq;
+        kq.tu = tu * a.tu;
+        kq.mau = mau * a.mau;
+       return kq;     
+    }
 phan_so::phan_so(){
     std::cout<<"khoi tao gia tri";
     int tu =0;
